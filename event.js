d453d134fc69c9fe6c56a77c2ed9ea700e30b9c0
1924458755@qq.com <1924458755@qq.com>
2017-10-11 18:21:09 +0800

    20171010 event,fs,http修改

--- event.js
+++ event.js
@@ -12,17 +12,29 @@ const eventEmitter = new events.EventEmitter();
 let handler = function(){
     console.log("hello boy");
 };
-//绑定事件以及事件的处理程序
+//on(event, listener)：为指定事件注册一个监听器
 eventEmitter.on("hello",handler);//为指定事件注册一个监听器，接受一个字符串 event 和一个回调函数
+
 //addListener(event, listener):为指定事件添加一个监听器到监听器数组的尾部。
 eventEmitter.addListener("hello",()=>{
     console.log("hello girl,再次监听hello事件");
 });
-eventEmitter.removeListener("hello",handler);
+
+//removeListener: 移除指定事件的某个监听器，监听器必须是该事件已经注册过的监听器
+//eventEmitter.removeListener("hello",handler);
+
+//removeAllListeners([event])：移除指定事件的所有监听器
+eventEmitter.removeAllListeners("hello");
+
 //once(event, listener):为指定事件注册一个单次监听器，即 监听器最多只会触发一次，触发后立刻解除该监听器。
 
 //触发事件
+//emit:按参数的顺序执行每个监听器
 eventEmitter.emit("hello");
+
+
+
+
 //示例二
 let connectHandler = function(){
     console.log("连接成功");
@@ -36,12 +48,19 @@ eventEmitter.on("data_received",receivedHandler);
 eventEmitter.emit("connect");
 console.log("程序执行结束");
 
+
+
 //多个监听器
 eventEmitter.on("changeEvent",(arg1,arg2)=>{
     console.log(`listener1 参数一：${arg1}；参数二：${arg2}`);
 });
-eventEmitter.on("changeEvent",(arg1,arg2)=>{
+eventEmitter.addListener("changeEvent",(arg1,arg2)=>{
     console.log(`listener2 参数一：${arg1}；参数二：${arg2}`);
 });
 eventEmitter.emit("changeEvent","name","age");
+//listenerCount(emitter, event): 返回指定事件的监听器数量。
+//
+let listenerArr = eventEmitter.listeners("changeEvent");
+console.log("监听数组：");
+console.info(listenerArr);
 console.log("程序执行结束");
